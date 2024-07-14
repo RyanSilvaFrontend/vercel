850c66a718eda10798a98bad4b4fdd96dd18814b
RyanSilvaFrontend <contateryansilva@gmail.com>
2024-07-14 01:52:31 -0300

    first commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Servir o arquivo HTML principal
-app.get('/', (req, res) => {
+app.get('/index.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
